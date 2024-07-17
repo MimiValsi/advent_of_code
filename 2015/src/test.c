a36986ecab1c8d7f87eaa8d167ec5f9daf42c5a4
MimiValsi <miguel@dasilvaf.net>
2024-07-17 18:58:24 +0200

    create linked list

--- 2015/src/test.c
+++ 2015/src/test.c
@@ -1,98 +1,76 @@
-#include <stdio.h>
-#include <string.h>
 #include "../misc/misc.h"
+#include <stdlib.h>
+#include <string.h>
 
-
-typedef struct Map {
+struct Link_list {
         char *key;
         u16 value;
-        struct Map *next;
-} Map_t;
+        struct Link_list *next;
+};
 
-void map_push(Map_t *head, char *str, u16 value);
-void map_print(Map_t *head);
-void map_free(Map_t *head);
-Map_t* map_search_key(Map_t *head, char *str);
+struct Link_list* map_init(struct Link_list *head, char const *str, u16 value);
+void map_push(struct Link_list *head, char *str, u16 value);
+void map_free(struct Link_list *head);
+void map_print(struct Link_list *head);
+struct Link_list* map_search_key(struct Link_list *head, char *str);
 
 int main(void)
 {
-        Map_t *head = malloc(sizeof(Map_t));
-        if (!head) {
-                return -1;
-        }
-        char *s = malloc(5);
-        // s = "Mike";
-        // s = strdup("Mike");
-        strcpy(s, "Mike");
-        printf("%s\n", s);
-        free(s); // <-- invalide pointer error
-
-        head->key = "Mimi";
-        head->value = 1;
-        printf("head.key = %s\n", head->key);
-        printf("head.value = %d\n", head->value);
-
-        free(head);
-
-
-
-        // head->key = "Mike";
-        // head->value = 10;
-        // head->next = NULL;
-
-        // map_push(head, "Baboue", 20);
-        // map_push(head, "Babou", 30);
-        // map_push(head, "Aure", 70);
-
-        // Map_t *tmp = map_search_key(head, "Baboue");
-        // printf("tmp->key = %s\n", tmp->key);
-        // printf("tmp->value = %d\n\n", tmp->value);
-        //
+        struct Link_list *head = NULL;
+        head = map_init(head, "mimi", 1);
         // map_print(head);
-        //
-        // map_free(head);
-        // free(head);
 
+        map_free(head);
         return 0;
 }
 
-void
-map_push(Map_t *head, char *str, u16 value)
+struct Link_list*
+map_init(struct Link_list *head, char const *str, u16 value)
 {
-        Map_t *current = head;
+        head = malloc(sizeof(*head));
+        head->key = strdup(str);
+        head->value = value;
 
-        while (current->next != NULL) {
+        return head;
+
+}
+
+void
+map_push(struct Link_list *head, char *str, u16 value)
+{
+        head = malloc(sizeof(*head));
+        struct Link_list *current = head;
+
+        while (current->next) {
                 current = current->next;
         }
 
-        current->next = malloc(sizeof(Map_t));
-        current->next->key = str;
-        // strcpy(current->next->key, str);
+        current->next = malloc(sizeof(*current));
+        current->next->key = malloc(64);
+        strcpy(current->next->key, str);
         current->next->value = value;
         current->next->next = NULL;
 }
 
 void
-map_print(Map_t *head)
+map_print(struct Link_list *head)
 {
-        Map_t *current = head;
-
-        while (current != NULL) {
+        struct Link_list *current = head;
+        while (current) {
                 printf("current->key = %s\n", current->key);
                 printf("current->value = %d\n", current->value);
-                NL();
+                // NL();
                 current = current->next;
         }
 }
 
-Map_t*
-map_search_key(Map_t *head, char *str)
+struct Link_list*
+map_search_key(struct Link_list *head, char *str)
 {
-        Map_t *current = head;
+        struct Link_list *current = head;
 
         while (current->next != NULL) {
                 if (strcmp(current->key, str) == 0) {
-                // if (current->key == str) {
                         return current;
                 }
                 current = current->next;
@@ -102,12 +80,13 @@ map_search_key(Map_t *head, char *str)
 }
 
 void
-map_free(Map_t *head)
+map_free(struct Link_list *head)
 {
-        Map_t *tmp;
-        while (head != NULL) {
+        struct Link_list *tmp = NULL;
+        while (head) {
                 tmp = head;
                 head = head->next;
+                free(tmp->key);
                 free(tmp);
         }
 }
